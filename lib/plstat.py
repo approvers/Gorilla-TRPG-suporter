1f38803bf61665518ec88cbce348422a12146558
loxygenk <loxygen.k@gmail.com>
2020-03-12 23:33:04 +0900

    †追加† 敵の情報をJSONから読めるようにした

--- lib/plstat.py
+++ lib/plstat.py
@@ -1,6 +1,7 @@
-import discord
+import json
 from random import randint
 
+import discord
 
 
 class StatusManager:
@@ -8,17 +9,19 @@ class StatusManager:
     ゲーム内のオブジェクト(プレイヤー,敵,GM)をつくるための親クラス
     直接このクラスのインスタンスを生成したりはしないでね♡
     """
+
     def __init__(self):
         """
         初期化処理を行う
         とりあえずすべてNoneの状態で param_initial param 辞書を宣言
         """
-        param_none          = {"kinryoku":None,"power":None,"chikara":None,"yasei":None,"banana":5,"GP":5,"HP":None}
-        self.param_initial  = param_none
-        self.param          = param_none
-        self.is_gm          = False
+        param_none = {"kinryoku": None, "power": None, "chikara": None,
+                      "yasei": None, "banana": 5, "GP": 5, "HP": None}
+        self.param_initial = param_none
+        self.param = param_none
+        self.is_gm = False
 
-    def get_status(self,param,is_initial=False):
+    def get_status(self, param, is_initial=False):
         """
         該当するステータス(現在値)を返してくれる
         Parameters
@@ -44,7 +47,6 @@ class StatusManager:
                 return self.param[param]
 
 
-
 class PlayerStatus(StatusManager):
     def __init__(self, message):
         """
@@ -60,7 +62,8 @@ class PlayerStatus(StatusManager):
         self.user = message.author
         self.id = message.author.id
         self.name = message.author.display_name
-    def set_status(self,message):
+
+    def set_status(self, message):
         """
         Playerの状態(初期状態)を定める
         !set 時に message を渡してインスタンス関数を呼び出してください
@@ -71,19 +74,16 @@ class PlayerStatus(StatusManager):
         """
         user_params = message.content.split()
         user_params.pop(0)
-        user_params = list(map(lambda x: int(x),user_params))
+        user_params = list(map(lambda x: int(x), user_params))
 
         if not len(user_params) == 4:
             return "ValueError"
         if not {2, 3, 4, 5} == set(user_params):
             return "StatusError"
-        self.param_initial["kinryoku"], self.param_initial["power"],self.param_initial["chikara"], self.param_initial["yasei"] = user_params
+        self.param_initial["kinryoku"], self.param_initial["power"], self.param_initial["chikara"], self.param_initial["yasei"] = user_params
         self.param_initial["HP"] = self.param_initial["power"] * 3 + 10
         self.param = self.param_initial
         return "Success"
-
-
-
 
 
 class EnemyStatus(StatusManager):
@@ -92,3 +92,22 @@ class EnemyStatus(StatusManager):
         self.id = randint(100000000000000000, 599999999999999999)
         self.type = "Enemy"
         # 念の為敵にも(たぶん)ユニークなidをもたせておく
+
+    @classmethod
+    def read_from_json(cls, name, file_path="statuses.json"):
+        status = StatusManager()
+
+        with open(file_path, mode="r") as f:
+            json_raw = json.loads(f.read())
+
+        status.param_initial["kinryoku"] = json_raw[name]["kinryoku"]
+        status.param_initial["power"] = json_raw[name]["power"]
+        status.param_initial["chikara"] = json_raw[name]["chikara"]
+        status.param_initial["yasei"] = json_raw[name]["yasei"]
+        status.param_initial["banana"] = json_raw[name]["banana"]
+        status.param_initial["GP"] = json_raw[name]["GP"]
+
+        status.param_initial["HP"] = status.param_initial["power"] * 3 + 10
+
+        status.param = status.param_initial.copy()
+        return status
